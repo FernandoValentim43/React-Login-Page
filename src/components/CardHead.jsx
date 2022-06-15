850d62ed90a9fd84bc5da8865654e2f14b8a82db
fernando <fernandoreal.valentim@gmail.com>
2022-06-15 15:40:37 -0300

    Fix: img path

--- src/components/CardHead.jsx
+++ src/components/CardHead.jsx
@@ -2,7 +2,7 @@ export default function CardHead(props) {
   return (
         <div className="card-head">
           <div className="img-section">
-            <img src="../src/headimg.png" />
+            <img src="..\src\styles\img.jpg"  alt="" />
           </div>
 
           <h2>{props.title}</h2>
