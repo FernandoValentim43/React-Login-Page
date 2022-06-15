2af97ea567580cf80e16194de9080215c8edb126
fernando <fernandoreal.valentim@gmail.com>
2022-06-15 15:30:25 -0300

    path fix

--- src/components/CardHead.jsx
+++ src/components/CardHead.jsx
@@ -2,7 +2,7 @@ export default function CardHead(props) {
   return (
         <div className="card-head">
           <div className="img-section">
-            <img src="../src/data.png" />
+            <img src="../src/headimg.png" />
           </div>
 
           <h2>{props.title}</h2>
